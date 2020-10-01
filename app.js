b91aa81d9197db0af446c267d9b7d12e852d51ba
shivaluma <shivaluma@gmail.com>
2020-10-01 11:52:00 +0700

    Add mongodb.

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
 const app = express();
 const cors = require('cors');
-
+const compression = require('compression');
+const mongoose = require('mongoose');
 app.use(cors());
+app.use(compression());
+
+mongoose.connect(
+  'mongodb+srv://momotalent2020:momotalent2020@cluster0.d4nvh.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  {
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) console.log('Connect database error');
+    else console.log('Connect database success');
+  }
+);
+
+
 
 app.listen(3000, () => {
   console.log('Server start on port 3000');
